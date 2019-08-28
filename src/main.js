39c54e4fd29601c9e94f44cfe2ac3c59dd321922
Yuetoo <1476104479@qq.com>
2019-08-28 11:12:09 +0800

    first git

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { Header } from "mint-ui";
+import "./lib/mui/css/mui.min.css";
 
 Vue.config.productionTip = false;
+Vue.component(Header.name, Header);
 
 new Vue({
   router,
